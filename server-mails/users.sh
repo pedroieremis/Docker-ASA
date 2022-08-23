2d12aca882f4778eed8e5bc3fa8b9badebd50dda
PedroIeremis <pedroieremis@gmail.com>
2022-08-22 23:58:05 -0300

    Otimizações

---
+++ server-mails/users.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Senha para usuário pedro"
+adduser -q --gecos " " pedro
+
+echo "Senha para usuário ieremis"
+adduser -q --gecos " " ieremis
+
+echo "Senha para usuário sales"
+adduser -q --gecos " " sales
